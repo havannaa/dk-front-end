abb29bb91eee5381235799d471687d35c8d1ecbc
Bishop Walker <bishop.walker@sbd2.com>
2023-09-12 09:31:07 -0400

    fixed something

--- src/components/headerTop/HeaderTop.tsx
+++ src/components/headerTop/HeaderTop.tsx
@@ -1,38 +1,35 @@
-import {Box, Grid, Typography} from "@mui/material";
-import {Email, Phone} from '@mui/icons-material';
+import { Box, Grid, Typography } from "@mui/material";
+import { Email, Phone } from '@mui/icons-material';
 import React from "react";
 import NNGCLogo from "../../assets/nngc-logo.png";
-
-import { Link } from 'react-router-dom'; // Import Link from React Router
-
-
+import { Link } from 'react-router-dom';
 
 const HeaderTop = () => {
 	return (
 		<Box sx={{ backgroundColor: 'white', color: '#777', borderTop: '5px solid #26C9FF', borderBottom: '5px solid #26C9FF', marginTop: 0 }}>
 			<Box>
-				<Grid container spacing={1} sx={{ padding: '8px', border: '1px solid grey' }}> {/* Reduced padding and spacing */}
+				<Grid container spacing={1} sx={{ padding: '8px', border: '1px solid grey' }}>
 					<Grid item xs={3} sm={3} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
 						<Link to="/">
-							<img src={NNGCLogo}
-								 alt="logo" style={{ height: '1.3rem' }} />
+							<img src={NNGCLogo} alt="logo" style={{ height: '1.3rem' }} />
 						</Link>
 					</Grid>
-					<Grid item xs={3} sm={3}>
-						<Typography variant="body1" textAlign="center" sx={{ lineHeight: '1rem' }}> {/* Adjusted line height */}
+					<Grid item xs={3} sm={3} sx={{ display: { xs: 'none', sm: 'none', lg: 'block', xl: 'block' } }}>
+						<Typography variant="body1" textAlign="center" sx={{ lineHeight: '1rem' }}>
 							We are Northumberland's #1 choice for trash removal!
 						</Typography>
 					</Grid>
-					<Grid item xs={3} sm={3}>
-						<Typography variant="body1" sx={{ display: 'flex', alignItems: 'center', lineHeight: '1rem' }}>
-							<Email sx={{ color: '#26C9FF', marginRight: '0.5rem' }} />
-							info@northernneckgarbage.com
-						</Typography>
+					<Grid item xs={3} sm={3} sx={{ display: { xs: 'none', sm: 'none', lg: 'flex', xl: 'flex' }, alignItems: 'center', lineHeight: '1rem' }}>
+						<Email sx={{ color: '#26C9FF', marginRight: '0.5rem' }} />
+						info@northernneckgarbage.com
 					</Grid>
-					<Grid item xs={3} sm={3}>
-						<Typography variant="body1" sx={{ display: 'flex', alignItems: 'center', lineHeight: '1rem' }}>
-							<Phone sx={{ color: '#26C9FF', marginRight: '0.5rem' }} />
-							804-435-1234
+					<Grid item xs={3} sm={3} sx={{ display: { xs: 'none', sm: 'none', lg: 'flex', xl: 'flex' }, alignItems: 'center', lineHeight: '1rem' }}>
+						<Phone sx={{ color: '#26C9FF', marginRight: '0.5rem' }} />
+						804-435-1234
+					</Grid>
+					<Grid item xs={9} sm={9} sx={{ display: { xs: 'block', sm: 'block', lg: 'none', xl: 'none' } }}>
+						<Typography variant="body1" textAlign="center" sx={{ lineHeight: '1rem' }}>
+							We are Northumberland's #1 choice for trash removal!
 						</Typography>
 					</Grid>
 				</Grid>
@@ -42,4 +39,5 @@ const HeaderTop = () => {
 };
 
 export default HeaderTop;
+
 
